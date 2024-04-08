218fa44dcf061acf756cc67ccb004f83fe7df59c
Javohir-mirolimov <githup@gmail.com>
2024-04-08 21:31:57 +0500

    javohir

--- main/models.py
+++ main/models.py
@@ -40,8 +40,10 @@ class Advantage(models.Model):
     title = models.CharField(max_length=55, verbose_name="sarlavfa")
     desciption = models.CharField(max_length=255, verbose_name="tarif")
     icon = models.ImageField(upload_to="advantage_icon", verbose_name="icon")
+
     def __str__(self):
         return self.title
+
     class Meta:
         verbose_name = 'Afzallik'
         verbose_name_plural = 'Afzalliklar'
@@ -49,5 +51,64 @@ class Advantage(models.Model):
 class Favorite_food(models.Model):
     desciption = models.CharField(max_length=255, verbose_name="tarif")
     icon = models.ImageField(upload_to="favorite_food/", verbose_name="icon")
+
+
+class Branch(models.Model):
+    name = models.CharField(max_length=55, verbose_name="nomi")
+    address = models.CharField(max_length=255, verbose_name="manzil")
+    start_time = models.TimeField(verbose_name="boshlanish vaqt")
+    end_time = models.TimeField(verbose_name="tugash vaqt")
+    lat = models.FloatField()
+    lot = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Filial'
+        verbose_name_plural = 'Filiallar'
+
+class Vacancy(models.Model):
+    title = models.CharField(max_length=55, verbose_name="sarlavfa")
+    text = models.TextField(verbose_name="to'liq malumot")
+    desciption = models.CharField(max_length=75 , verbose_name="tarif")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Ish haqida malomut'
+        verbose_name_plural = 'Ishlar haqida malomut'
+
+class Send_request(models.Model):
+    fullname = models.CharField(max_length=255, verbose_name="ism va falimiya")
+    phone_number = models.CharField(max_length=13, verbose_name="telefon raqam")
+    image = models.ImageField(upload_to="send_request_photo")
+
+    def __str__(self):
+        return self.fullname
+
+    class Meta:
+        verbose_name = 'Ariza'
+        verbose_name_plural = 'Arizalar'
+
+
+class Job_benefit(models.Model):
+    title = models.CharField(max_length=35, verbose_name="sarlavha")
+    text = models.TextField(verbose_name="toliq malomot")
+    icon = models.ImageField(upload_to="job_benefit_icon/" , verbose_name="icon")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'ish avfzalik'
+        verbose_name_plural = 'ish avfzaliklari'
+
+
+class Career(models.Model):
+
+
+
 
     
